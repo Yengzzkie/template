4f2fa19d09aa836d9a595e6afdd7977413bb33ab
Yengzzkie <gatchalian.manuel@gmail.com>
2024-03-31 13:38:58 -0400

    feature/added location

--- src/modules/getCurrentWeather.js
+++ src/modules/getCurrentWeather.js
@@ -1,6 +1,13 @@
 import getWeatherData from './getWeatherData';
+import Overcast from '../assets/Overcast.png';
 
 export default async function getCurrentWeather() {
+  const temp = document.querySelector('.temp');
+  const feelsLikeTemp = document.querySelector('.feelslike');
+  const condition = document.querySelector('.condition');
+  const minmax = document.querySelector('.minmax');
+  const weatherIcon = document.querySelector('.weather-image');
+
   try {
     const data = await getWeatherData();
     const currentWeather = data.current;
@@ -9,15 +16,24 @@ export default async function getCurrentWeather() {
     const lastUpdate = currentWeather.last_updated;
     console.log(lastUpdate);
 
-    const weatherCondition = currentWeather.condition.text;
-    console.log(weatherCondition);
-
     const weatherTemp = currentWeather.temp_c;
-    console.log(`${weatherTemp} Celsius`);
+    console.log(`${weatherTemp}°`);
+    temp.textContent = `${weatherTemp}°`;
 
     const feelsLike = currentWeather.feelslike_c;
+    feelsLikeTemp.textContent = `Feels like ${feelsLike} Celsius`;
     console.log(`Feels like ${feelsLike} Celsius`);
+
+    const weatherCondition = currentWeather.condition.text;
+    console.log(weatherCondition);
+    condition.textContent = weatherCondition;
+
+    const weatherConditionIcon = currentWeather.condition.icon;
+    weatherIcon.innerHTML = `<img src=${weatherConditionIcon}>`;
+
   } catch (err) {
     console.error('Something went wrong:', err);
   }
+
 }
+
--- src/modules/getLocation.js
+++ src/modules/getLocation.js
@@ -1,9 +1,13 @@
 import getWeatherData from './getWeatherData';
 
 export default async function getLocation() {
+  const location = document.querySelector('.location');
+
   try {
     const data = await getWeatherData();
     const locationData = data.location;
+
+    location.innerHTML = `${locationData.name}, ${locationData.country} <i class="fa-solid fa-location-dot"></i>`;
 
     console.log(locationData.name, locationData.country);
   } catch (err) {
