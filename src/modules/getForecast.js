de118bd165af02b08cf8809e83b424a8f77f2858
Yengzzkie <gatchalian.manuel@gmail.com>
2024-04-01 17:32:55 -0400

    feature/added 7day forecast

--- src/modules/getForecast.js
+++ src/modules/getForecast.js
@@ -3,13 +3,20 @@ import getWeatherData from './getWeatherData';
 export default async function getForecast() {
   try {
     const data = await getWeatherData();
-
+    const forecastSection = document.getElementById('forecast-section')
+    const forecastContainer = document.querySelector('.forecast-container');
     const sevenDayForecast = data.forecast.forecastday;
     console.log(sevenDayForecast);
 
     sevenDayForecast.forEach((weatherData) => {
       console.log(`Date: ${weatherData.date}, Condition: ${weatherData.day.condition.text}, Average Temp: ${weatherData.day.avgtemp_c} C, Min-max Temp ${weatherData.day.mintemp_c} C - ${weatherData.day.maxtemp_c} C`);
+      const dayCard = document.createElement('div');
+
+      dayCard.textContent = `Date: ${weatherData.date}, Condition: ${weatherData.day.condition.text}, Average Temp: ${weatherData.day.avgtemp_c} C, Min-max Temp ${weatherData.day.mintemp_c} C - ${weatherData.day.maxtemp_c} C`;
+      forecastContainer.append(dayCard)
     });
+    forecastSection.append(forecastContainer);
+    
   } catch (err) {
     console.error('Something went wrong:', err);
   }
